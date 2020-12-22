081fbc8e2d8d9608a2e63b6a9fd7e859985fa30e
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2020-12-22 12:56:04 -0500

    Basic Website Outline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import { React, Component } from 'react'; 
-import './App.css';
+import React, {Component, Fragment} from 'react'; 
+import Navbar from './Navbar'; 
+import OptionsPanel from './OptionsPanel'; 
+import ChatBox from './ChatBox'; 
 
-export default class App extends React.Component {
-  return (
+export default class App extends Component {
+  render() {
+    const divStyle = {
+      width: '100%',
+      height: 'calc(100% - 60px)',
+      display: 'inline-block',
+      paddingTop:'60px'
+    };
 
-  ); 
+    return (
+      <Fragment>
+        <Navbar />
+        <div style = {divStyle}>
+          <OptionsPanel />
+          <ChatBox />
+        </div>
+      </Fragment>
+    );
+  }
 }
