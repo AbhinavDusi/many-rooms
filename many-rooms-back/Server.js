2c171b028e95f8ffcd0cbf709360e3c531b53c86
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2020-12-29 17:39:19 -0500

    Added Create Party

--- many-rooms-back/Server.js
+++ many-rooms-back/Server.js
@@ -1,13 +1,16 @@
 const Express = require('express'); 
+const bodyParser = require('body-parser');
 const mysql = require('mysql'); 
 
 const app = Express(); 
+const jsonParser = bodyParser.json(); 
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '6Fsb2002',
-    database: 'many_rooms'
+    database: 'many_rooms',
+    multipleStatements: true
 }); 
 
 db.connect();
@@ -33,11 +36,71 @@ app.get('/f/:floor', (req, res) => {
 }); 
 
 app.get('/profile/:id', (req, res) => {
-
+    let sqlQuery = ``;
 }); 
 
 app.get('/p/:id', (req, res) => {
     let sqlQuery = ``;
 }); 
+
+app.post('/createparty', jsonParser, (req, res) => {
+    if (req.body.titleValue.length < 5) {
+        res.json({
+            err: 1,
+            msg: 'The title is too short.'
+        });
+        return;
+    } else if (req.body.titleValue.length > 25) {
+        res.json({
+            err: 1,
+            msg: 'The title is too long.'
+        });
+        return;
+    } else if (req.body.bodyValue.length < 25) {
+        res.json({
+            err: 1,
+            msg: 'The body is too short.'
+        }); 
+        return;
+    } else if (req.body.bodyValue.length > 500) {
+        res.json({
+            err: 1,
+            msg: 'The body is too long.'
+        }); 
+        return; 
+    }
+    let sqlQuery = `
+        INSERT INTO parties (
+            host_id,
+            title,
+            attendees,
+            posts,
+            tags,
+            status,
+            floor,
+            time_limit
+        )
+        VALUES (
+            ${0},
+            '${req.body.titleValue}',
+            ${1},
+            ${1},
+            '${req.body.tagsValue}',
+            ${1},
+            '${req.body.floor}',
+            '${req.body.timeValue}'
+        );
+        SELECT 
+            LAST_INSERT_ID() party_id;
+    `;
+    db.query(sqlQuery, (err, result) => {
+        res.json({
+            err: 0,
+            msg: {
+                partyID: result[1][0].party_id
+            }
+        }); 
+    }); 
+});
 
 app.listen(5000); 
--- many-rooms-front/src/Screens/CreateRoomScreen.js
+++ many-rooms-front/src/Screens/CreateRoomScreen.js
@@ -2,25 +2,74 @@ import React, { Component } from 'react';
 import { outerDivStyle, innerDivStyle, mainHeader, infoText, inputTextStyle, 
     bodyTextAreaStyle, buttonStyle, selectStyle } from './ScreenStyles';
 
-export default class ChatBoxScreen extends Component {
+export default class CreateRoomScreen extends Component {
+    state = {
+        titleValue: '',
+        tagsValue: '',
+        bodyValue: '',
+        timeValue: 5,
+        errMsg: ''
+    }
+
+    handleCreateParty = () => {
+        const partyInfo = {...this.state}; 
+        partyInfo.floor = window.location.pathname.split("/")[2]; 
+        fetch('/createparty', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(partyInfo)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.err === 1) {
+                    this.setState({errMsg: result.msg});
+                } else {
+                    window.location.pathname = '/p/' + result.msg.partyID;
+                }
+            });
+    }
+
     render() {
         return (
             <div style = {outerDivStyle}>
                 <div style = {innerDivStyle}>
                     <p style = {mainHeader}>Create A Party</p>
                     <p style = {infoText}>Title</p>
-                    <input type = 'text' style = {inputTextStyle} />
+                    <input 
+                        type = 'text' 
+                        style = {inputTextStyle} 
+                        onChange = {e => this.setState({titleValue: e.target.value})}
+                    />
                     <p style = {infoText}>Tags (Separated by a comma)</p>
-                    <input type = 'text' style = {inputTextStyle} />
+                    <input 
+                        type = 'text' 
+                        style = {inputTextStyle} 
+                        onChange = {e => this.setState({tagsValue: e.target.value})}
+                    />
                     <p style = {infoText}>Body</p>
-                    <textarea style = {bodyTextAreaStyle}></textarea>
-                    <button style = {{marginTop: '25px',...buttonStyle}}>Submit</button>
+                    <textarea 
+                        style = {bodyTextAreaStyle}
+                        onChange = {e => this.setState({bodyValue: e.target.value})}
+                    ></textarea>
+                    <button 
+                        style = {{marginTop: '25px',...buttonStyle}}
+                        onClick = {this.handleCreateParty}
+                    >
+                        Submit
+                    </button>
+                    <span style = {{marginLeft: '15px'}}>
+                        {this.state.errMsg}
+                    </span>
                     <p style = {infoText}>
                         Time between messages:
-                        <select style = {selectStyle}>
-                            <option>5 seconds</option>
-                            <option>10 seconds</option>
-                            <option>30 seconds</option>
+                        <select 
+                            style = {selectStyle}
+                            onChange = {e => this.setState({timeValue: parseInt(parseInt(e.target.value))})}
+                            value = {this.state.timeValue}
+                        >
+                            <option value = '5'>5 seconds</option>
+                            <option value = '10'>10 seconds</option>
+                            <option value = '30'>30 seconds</option>
                         </select>
                     </p>
                 </div>
